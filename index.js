9d5131e3f57b203a311718028f64561c646be5ff
unknown <hannanmalik1999@gmail.com>
2022-08-08 15:47:00 +0200

    Project Structured and Background Image Fetched

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-.then(res => res.json())
-.then(data => {
-    document.body.style.backgroundImage = `url(${data.urls.raw})`;
+    .then(res => res.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+		document.getElementById("author").textContent = `By: ${data.user.name}`
     })
+
