2838c530420b2aac740d6e57df36b14d03bd4080
unknown <hannanmalik1999@gmail.com>
2022-08-08 17:10:52 +0200

    Time Updated

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
 		document.getElementById("author").textContent = `By: ${data.user.name}`
     })
     .catch(err => {
+        // Use a default background image/author
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
 )`
 		document.getElementById("author").textContent = `By: Dodi Achmad`
     })
 
-fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     .then(res => {
         if (!res.ok) {
             throw Error("Something went wrong")
@@ -23,9 +24,16 @@ fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
             <span>${data.name}</span>
         `
         document.getElementById("crypto").innerHTML += `
-            <p>Current: $${data.market_data.current_price.usd}</p>
-            <p>24h High: $${data.market_data.high_24h.usd}</p>
-            <p>24h Low: $${data.market_data.low_24h.usd}</p>
+            <p>🎯: $${data.market_data.current_price.usd}</p>
+            <p>👆: $${data.market_data.high_24h.usd}</p>
+            <p>👇: $${data.market_data.low_24h.usd}</p>
         `
     })
     .catch(err => console.error(err))
+
+function getCurrentTime() {
+    const date = new Date()
+    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
+}
+
+setInterval(getCurrentTime, 1000)
