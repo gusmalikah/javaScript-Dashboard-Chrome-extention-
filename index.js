93ac4e552976399db631005bdbe6beb433d115d3
unknown <hannanmalik1999@gmail.com>
2022-08-08 18:39:03 +0200

    Functionality Added and Deployed

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
+		document.getElementById("author").textContent = `Photo By: ${data.user.name}`
     })
     .catch(err => {
         // Use a default background image/author
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
 )`
-		document.getElementById("author").textContent = `By: Dodi Achmad`
+		document.getElementById("author").textContent = `Photo By: Dodi Achmad`
     })
 
-fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
     .then(res => {
         if (!res.ok) {
             throw Error("Something went wrong")
@@ -24,9 +24,9 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
             <span>${data.name}</span>
         `
         document.getElementById("crypto").innerHTML += `
-            <p>🎯: $${data.market_data.current_price.usd}</p>
-            <p>👆: $${data.market_data.high_24h.usd}</p>
-            <p>👇: $${data.market_data.low_24h.usd}</p>
+            <p>Current: $${data.market_data.current_price.usd}</p>
+            <p>24h High: $${data.market_data.high_24h.usd}</p>
+            <p>24h Low: $${data.market_data.low_24h.usd}</p>
         `
     })
     .catch(err => console.error(err))
@@ -37,3 +37,23 @@ function getCurrentTime() {
 }
 
 setInterval(getCurrentTime, 1000)
+
+navigator.geolocation.getCurrentPosition(position => {
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error("Weather data not available")
+            }
+            return res.json()
+        })
+        .then(data => {
+            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            document.getElementById("weather").innerHTML = `
+                <img src=${iconUrl} />
+                <p class="weather-temp">${Math.round(data.main.temp)}°</p>
+                <p class="weather-city">${data.name}</p>
+            `
+        })
+        .catch(err => console.error(err))
+});
+
